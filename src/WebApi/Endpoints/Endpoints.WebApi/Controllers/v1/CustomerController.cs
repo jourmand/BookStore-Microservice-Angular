09eb862ed5afa04f39aeaad13c87ea1c055f036b
bakhtiary <Bakhtiary.amir@hotmail.com>
2021-10-19 14:56:59 +0330

    edit

---
+++ src/WebApi/Endpoints/Endpoints.WebApi/Controllers/v1/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit.Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoints.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
